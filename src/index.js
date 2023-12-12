9d6a8b65ee5a9ae90e7fffe8bb79a8405e0d0e7a
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2023-12-12 18:30:07 -0500

    Modificaciones a index.html, style.css e index.js En el fichero html modifique: -Movi el <script index.js> al final de <body> -Elimine el <script analyzer.js> -Agregue un atributo type a la etiqueta <button>. En el fichero index.js modifique: -Agregue los eventos del boton limpiar metricas. -Para limpiar tanto la caja de texto como las metricas. -Use los metodos getElementById() y querySelector(). No hubo cambios relevantes en analyzer.js y style.css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
+
+const clkLimpiar = document.getElementById("reset-button");
+const limpiarTexto = document.querySelector("textarea");
+const limpiarMetricas = document.querySelector(".box");
+
+clkLimpiar.addEventListener("click", function Limpiar() {
+  limpiarTexto.value= "";
+  limpiarMetricas.value= "";
+})
