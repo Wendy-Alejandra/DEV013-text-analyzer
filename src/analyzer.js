6bc322f8eca3dbd60e57d03f35c69b10cc6b0bf7
Wendy Chirinos <89660705+Wendy-Alejandra@users.noreply.github.com>
2023-12-15 16:09:50 -0500

    Index.js y style.css Modificados En Index.js: Agregue la funcion que limpia las metricas con un for y textcontent pero no me actualiza a cero. En Style.css: -Agregué textarea[name="atributo"] porque el test individual no me lo aceptó sin el textarea. -Agregue tambien el # al id del boton que no lo habia agregado. -Ahora las cajas con las metricas se mueven de lugar, antes no lo hacian.

--- src/analyzer.js
+++ src/analyzer.js
@@ -1,10 +1,19 @@
 const analyzer = {  
   getWordCount: (text) => {
-
     //TODO: esta función debe retornar el recuento de palabras que se encuentran en el parámetro `text` de tipo `string`.
+    //PASOS: 1. Obtener texto, 2. Partir el texto en porciones, 3. Contar palabras, 4. Mostrar resultado
+    const wordCount = text.split(" ").length;
+    console.log(wordCount);
+    // if (wordCount === 0) {
+    //   document.write("word-count: 0")
+    // } else
+      return wordCount;
   },
+
   getCharacterCount: (text) => {
     //TODO: esta función debe retornar el recuento de caracteres que se encuentran en el parámetro `text` de tipo `string`.
+    //PASOS: 1. Obtener texto, 2. Contar caracteres, 3. Mostrar resultado
+  
   },
   getCharacterCountExcludingSpaces: (text) => {
 
--- src/index.js
+++ src/index.js
@@ -2,11 +2,66 @@ import analyzer from './analyzer.js';
 
 //TODO: escuchar eventos del DOM e invocar  los métodos del objeto `analyzer`
 
-const clkLimpiar = document.getElementById("reset-button");
-const limpiarTexto = document.querySelector("textarea");
-const limpiarMetricas = document.querySelector(".box");
+// Declaración de constantes para acceder a los elementos y atributos de HTML
+const actualizarInputs = [
+  document.querySelector("li[data-testid='word-count']"),
+  document.querySelector("li[data-testid='character-count']"),
+  document.querySelector("li[data-testid='character-no-spaces-count']"),
+  document.querySelector("li[data-testid='number-count']"),
+  document.querySelector("li[data-testid='number-sum']"),
+  document.querySelector("li[data-testid='word-length-average']")];
+console.log(actualizarInputs);
 
-clkLimpiar.addEventListener("click", function Limpiar() {
-  limpiarTexto.value= "";
-  limpiarMetricas.value= "";
-})
+// for (actualizarInputs=0; actualizarInputs < 6; i++)
+// console.log(actualizarInputs);
+// const limpiarMetricas = document.querySelector(".box");
+// const textoEntrada = document.querySelector("user-input");
+// const wordCount = document.querySelector("li[data-testid='word-count']");
+// const characterCount = document.querySelector("li[data-testid='character-count']");
+// const characterNoSpacesCount = document.querySelector("li[data-testid='character-no-spaces-count']");
+// const numberCount = document.querySelector("li[data-testid='number-count']");
+// const numberSum = document.querySelector("li[data-testid='number-sum']");
+// const wordLengthAverage = document.querySelector("li[data-testid='word-length-average']")
+const cajaTexto = document.querySelector("textarea");
+const clkLimpiar = document.getElementById("reset-button");
+
+// Actualizar Métricas con el evento input del <textarea>
+function mostrarMetricas() {
+  const text = cajaTexto.value;
+  document.querySelector("li[data-testid='word-count']").innerHTML= "word-count: " + analyzer.getWordCount(text);
+// characterCount.innerHTML= "character-count: " + analyzer.getCharacterCount(text);
+// characterNoSpacesCount.innerHTML= "character-no-spaces-count: " + analyzer.getCharacterCountExcludingSpaces(text);
+// numberCount.innerHTML= "number-count: " + analyzer.getNumberCount(text);
+// numberSum.innerHTML= "number-sum: " + analyzer.getNumberSum(text);
+// wordLengthAverage.innerHTML= "word-length-average: " + analyzer.getAverageWordLength(text);
+}
+
+// function mostrarMetricas() {
+//   const text = cajaTexto.value;
+//     }
+// }
+
+// Limpiar contenido de la caja de texto y métricas
+function limpiar() {
+  cajaTexto.value= "";
+  for(let i=0; i < 6; i++) {
+    console.log(actualizarInputs[i]);
+    actualizarInputs[i].innerHTML= actualizarInputs[i].textContent;
+  }
+}
+
+// function limpiarWordCount() {
+//   wordCount.value ="";
+// }
+
+cajaTexto.addEventListener("input", mostrarMetricas);
+clkLimpiar.addEventListener("click", limpiar);
+// wordCount.addEventListener("click", limpiarWordCount);
+
+// const cargarPagina = location.reload();
+// console.log(cargarPagina);
+
+// // console.log(clkLimpiar);
+// console.log(analyzer.getWordCount("Hola Mundo de Dios"));
+// // console.log(actualizarInputs[0]);
+// console.log(actualizarMetricas())
